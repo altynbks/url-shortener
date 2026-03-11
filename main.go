00b1a525c8a022612a610fd40a4850617863e1b1
altynbks <altynbekxv@gmail.com>
2026-03-11 18:14:59 +0500

    lalala

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"math/rand/v2"
 	"net/http"
+	"sync"
 )
 
 type Url struct {
@@ -11,7 +13,16 @@ type Url struct {
 	Short string `json:"short"`
 }
 
-func ShortenHandler(w http.ResponseWriter, r *http.Request) {
+type UrlStore struct {
+	urls map[string]string
+	mu   sync.RWMutex
+}
+
+type Server struct {
+	store *UrlStore
+}
+
+func (s *Server) ShortenHandler(w http.ResponseWriter, r *http.Request) {
 	var data Url
 	if r.Method != http.MethodPost {
 		http.Error(w, "Only POST is allowed", http.StatusMethodNotAllowed)
@@ -26,20 +37,77 @@ func ShortenHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Url is required", http.StatusBadRequest)
 		return
 	}
-	result := generateKey(67)
 
+	key := s.store.Set(data.Long)
+	data.Short = key
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(result)
+	json.NewEncoder(w).Encode(data)
+}
+
+func (s *Server) RedirectHandler(w http.ResponseWriter, r *http.Request) {
+	if len(r.URL.Path) < 4 {
+		http.Error(w, "Invalid ID", http.StatusBadRequest)
+		return
+	}
+	shortKey := r.URL.Path[3:]
+	longURL, ok := s.store.Get(shortKey)
+	if !ok {
+		http.Error(w, "URL not found", http.StatusNotFound)
+		return
+	}
+	http.Redirect(w, r, longURL, http.StatusFound)
 }
 
 func generateKey(n int) string {
-	const alphanumeric = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	alphanumeric := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	res := make([]byte, n)
+	for i := range res {
+		randomIndex := rand.IntN(len(alphanumeric))
+		selected := alphanumeric[randomIndex]
+		res[i] = selected
+	}
+	return string(res)
+}
 
-	return "abc123"
+func NewUrlStore() *UrlStore {
+	return &UrlStore{
+		urls: make(map[string]string),
+	}
+}
+
+func (s *UrlStore) Set(longURL string) string {
+	// 1. Заблокируй на запись
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	// 2. Сгенерируй ключ через свою функцию generateKey
+	key := generateKey(6)
+	// 3. Сохрани в s.urls
+	s.urls[key] = longURL
+	// 4. Верни ключ
+	return key
+}
+
+func (s *UrlStore) Get(shortKey string) (string, bool) {
+	// 1. Заблокируй на ЧТЕНИЕ (RLock)
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+	// 2. Достань ссылку из мапы
+	val, ok := s.urls[shortKey]
+	if ok {
+		return val, true
+	}
+	// 3. Верни ссылку и флаг "найдено/не найдено"
+	return "", false
 }
 
 func main() {
-	http.HandleFunc("/shorten", ShortenHandler)
+	store := NewUrlStore()
+
+	srv := &Server{
+		store: store,
+	}
+	http.HandleFunc("/shorten", srv.ShortenHandler)
+	http.HandleFunc("/r/", srv.RedirectHandler)
 
 	fmt.Println("Listening and serving on:8080")
 
@@ -47,7 +115,3 @@ func main() {
 		fmt.Println(err)
 	}
 }
-
-//что бы получить короткую строку из числа и строки нам необходимо использовать base62 encoding.
-//что бы из числа получить строку нам сначала надо вычислить остаток от деления каждой цифры в числе. затем мы возводим в степень 62 на остаток. получаем сумму
-//а с строкой мы делаем следующее : мы сначала получаем байтовое значение, потом переводим это в hex, затем его в base62
